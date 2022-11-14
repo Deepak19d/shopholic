268372904a92db310a22dcf815689ddb37c54d1c
Deepak kumar <88934002+Deepak19d@users.noreply.github.com>
2022-11-14 21:53:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+ $(document).ready(function(){
+
+$('.heart-icon').click(function(){
+    $('.heart-icon').toggleClass('xyz');
+})
+
+$('.heart-icon-1').click(function(){
+    $('.heart-icon-1').toggleClass("xyz");
+})
+
+$('.heart-icon-2').click(function(){
+    $('.heart-icon-2').toggleClass("xyz");
+})
+
+$('.heart-icon-3').click(function(){
+    $('.heart-icon-3').toggleClass("xyz");
+})
+
+
+$('.heart-icon-4').click(function(){
+    $('.heart-icon-4').toggleClass("xyz");
+})
+
+$('.heart-icon-5').click(function(){
+    $('.heart-icon-5').toggleClass("xyz");
+})
+ });
+
+
